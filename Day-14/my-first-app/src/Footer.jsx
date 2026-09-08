02e235dc92ea2ad3e74282ffc7f127237ef0e3aa
Eliot <fulieliot658@gmail.com>
2026-09-08 16:43:05 +0100

    W8 D2(05)

---
+++ Day-14/my-first-app/src/Footer.jsx
@@ -0,0 +1,6 @@
+import '.App.css'
+function Footer(){
+return <footer>
+&copy; 2023 My App. All rights reserved.</footer>;
+}
+export default Footer;
---
+++ Day-14/my-first-app/src/Greeting.jsx
@@ -0,0 +1,5 @@
+import './App.css'
+function Greeting(){
+return <h2>Bonjour Michael!</h2>;
+}
+export default Greeting;
